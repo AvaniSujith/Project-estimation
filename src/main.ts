e1ce77451b6815202d8e5f6874717488c3c24b03
AvaniSujith <chinchuavani123@gmail.com>
2025-07-11 17:24:31 +0530

    chore: install vuetify into project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
 
-createApp(App).mount('#app')
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import "./style.css";
+import App from "./App.vue";
+
+const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(vuetify);
+app.mount("#app");
