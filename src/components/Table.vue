a57902b347914f2b146a6c2883dea330a4125149
AvaniSujith <chinchuavani123@gmail.com>
2025-07-14 13:59:31 +0530

    test: table component

---
+++ src/components/Table.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+ const items = [
+    {
+      name: 'African Elephant',
+      species: 'Loxodonta africana',
+      diet: 'Herbivore',
+      habitat: 'Savanna, Forests',
+    },
+  ]
+</script>
+<template>
+    <v-date-table :items="items" ></v-date-table>
+</template>
