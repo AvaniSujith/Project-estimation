89922b5bd4ff8bb621e778a8da2ccb7358d18884
AvaniSujith <chinchuavani123@gmail.com>
2025-07-14 09:56:16 +0530

    test: inputBar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import InputField from "./components/InputField.vue";
+const title = ref("");
+</script>
+<template>
+  <input-field
+    label="Estimation title"
+    v-model="title"
+    :required="true"
+  />
+  <p>{{ title }}</p>
+</template>
---
+++ src/components/InputField.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface Props{
+    label:string,
+    modelValue: string | number,
+    required?: boolean,
+    disabled?: boolean,
+}
+
+withDefaults( defineProps<Props>(),{
+  disabled : false,
+  required : false,
+})
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string | number): void
+}>();
+
+const handleInput = (event: Event) => {
+  const value = (event.target as HTMLInputElement).value;
+  emit("update:modelValue", value);
+}
+
+</script>
+<template>
+  <div>
+    <div class="text-subtitle-1 text-medium-emphasis">{{ label }}</div>
+    <v-text-field
+      density="compact"
+      type="text"
+      variant="outlined" 
+      :disabled="disabled"
+      :required="required"
+      :value="modelValue"
+      @input="handleInput"
+    ></v-text-field>
+  </div>
+</template>
+
+<style scoped>
+</style> 
