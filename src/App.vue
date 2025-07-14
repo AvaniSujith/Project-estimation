674d9948bff4ff7b2e6958ee6efb5fd376d02042
AvaniSujith <chinchuavani123@gmail.com>
2025-07-14 10:30:24 +0530

    feat: reusable input bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,2 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import InputField from "./components/InputField.vue";
-const title = ref("");
-</script>
-<template>
-  <input-field
-    label="Estimation title"
-    v-model="title"
-    :required="true"
-  />
-  <p>{{ title }}</p>
-</template>
+<script setup lang="ts"></script>
+<template></template>
--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,40 +1,37 @@
 <script setup lang="ts">
-interface Props{
-    label:string,
-    modelValue: string | number,
-    required?: boolean,
-    disabled?: boolean,
+interface Props {
+  label: string;
+  modelValue: string | number;
+  required: boolean;
+  disabled?: boolean;
 }
 
-withDefaults( defineProps<Props>(),{
-  disabled : false,
-  required : false,
-})
+const props = withDefaults(defineProps<Props>(), {
+  disabled: false,
+  required: false,
+});
 
 const emit = defineEmits<{
-  (e: "update:modelValue", value: string | number): void
+  (e: "update:modelValue", value: string | number): void;
 }>();
 
-const handleInput = (event: Event) => {
-  const value = (event.target as HTMLInputElement).value;
+const handleInput = (value: string | number) => {
   emit("update:modelValue", value);
-}
-
+};
 </script>
+
 <template>
   <div>
     <div class="text-subtitle-1 text-medium-emphasis">{{ label }}</div>
+
     <v-text-field
       density="compact"
       type="text"
-      variant="outlined" 
-      :disabled="disabled"
-      :required="required"
-      :value="modelValue"
-      @input="handleInput"
-    ></v-text-field>
+      variant="outlined"
+      :model-value="modelValue"
+      :disabled="props.disabled"
+      :required="props.required"
+      @update:model-value="handleInput"
+    />
   </div>
 </template>
-
-<style scoped>
-</style> 
