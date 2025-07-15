1f2f362d7407460ae47860db7bcef471cb9dd937
AvaniSujith <chinchuavani123@gmail.com>
2025-07-15 11:40:32 +0530

    feat: label component

---
+++ src/components/TitleText.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+<template>
+  <div class="text-subtitle-1 text-medium-emphasis" :title="title">
+    {{ title }}
+  </div>
+</template>
