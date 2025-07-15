08308e59e75715ececfd792a24e813412699fc23
AvaniSujith <chinchuavani123@gmail.com>
2025-07-15 10:56:47 +0530

    test: rules

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,11 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import InputField from './components/InputField.vue';
+
+const title = ref('')
+</script>
+<template>
+  <input-field v-model="title" placeholder=" eg.,type here"
+  />
+  <p>{{ title }}</p>
+</template>
--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,37 +1,55 @@
 <script setup lang="ts">
 interface Props {
-  label: string;
   modelValue: string | number;
+  placeholder: string;
   required?: boolean;
   disabled?: boolean;
+  minLength?: number | null;
+  maxLength?: number | null;
 }
 
 withDefaults(defineProps<Props>(), {
   disabled: false,
   required: false,
+  minLength: null,
+  maxLength: null,
 });
 
 const emit = defineEmits<{
-  (e: "update:modelValue", value: string | number): void;
+  (e: "update:modelValue", value: string): void;
 }>();
 
-const handleInput = (value: string | number) => {
+const customRule =(value: string) => {
+  if ((value === null) | (value === undefined) | (value === "")) {
+    rules.unshift[(value.required, "The input field cannot be empty")];
+  }
+
+  if (value.length < minLength && value.length > maxLength) {
+    RulesSymbol.unshift[
+      (minLength,
+      maxLength,
+      `The input should have charcter count between ${minLength} - ${maxLength}`)
+    ];
+  }
+};
+
+const handleInput = (value: string) => {
   emit("update:modelValue", value);
 };
 </script>
 
 <template>
-  <div>
-    <div class="text-subtitle-1 text-medium-emphasis">{{ label }}</div>
-
-    <v-text-field
-      density="compact"
-      type="text"
-      variant="outlined"
-      :model-value="modelValue"
-      :disabled="disabled"
-      :required="required"
-      @update:model-value="handleInput"
-    />
-  </div>
+  <v-text-field
+    density="compact"
+    type="text"
+    variant="outlined"
+    :model-value="modelValue"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    :required="required"
+    :rules="customRule"
+    :min-length="minLength"
+    :max-length="maxLength"
+    @update:model-value="handleInput"
+  ></v-text-field>
 </template>
