dceb13d59de1e76a9dbd2cb736ffb0454853afa9
AvaniSujith <chinchuavani123@gmail.com>
2025-07-15 11:30:44 +0530

    refactor: code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,2 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import InputField from './components/InputField.vue';
-
-const title = ref('')
-</script>
-<template>
-  <input-field v-model="title" placeholder=" eg.,type here" :min-length="5" :max-length="26" :required="false"
-  />
-  <p>{{ title }}</p>
-</template>
+<script setup lang="ts"></script>
+<template></template>
