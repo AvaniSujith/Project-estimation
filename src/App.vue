323bcfff7216f412e7ed914af44e12e59e9675c2
AvaniSujith <chinchuavani123@gmail.com>
2025-07-15 21:57:54 +0530

    test: dropdown component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import DropDown from "./components/DropDown.vue";
-const items = ["item1", "item2", "item3"];
+const items = [
+  { item: 'Item 1', value: 'item1' },
+  { item: 'Item 2', value: 'item2' },
+  { item: 'Item 3', value: 'item3' },
+];
 </script>
 
 <template>
-  <drop-down :items="item in items" />
+  <drop-down :items="items" />
 </template>
