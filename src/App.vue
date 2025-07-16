517e9decbbf91afaff629f9c0c29627327ce269c
AvaniSujith <chinchuavani123@gmail.com>
2025-07-16 14:40:06 +0530

    refactor: code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,2 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import DropDown from "./components/DropDown.vue";
-
-const items = [
-  { item: "Item 1", value: "item1" },
-  { item: "item 2", value: "item2" },
-  { item: "item 3", value: "item3" },
-];
-
-const itemSelected = ref<string>("select");
-
-</script>
-<template>
-  <DropDown
-    :items="items"
-    v-model="itemSelected"
-  />
-  <div>{{ itemSelected }}</div>
-</template>
- 
+<script setup lang="ts"></script>
+<template></template>
