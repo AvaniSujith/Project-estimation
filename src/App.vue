41dc24bf27220a95fa72dce880422090a319277c
AvaniSujith <chinchuavani123@gmail.com>
2025-07-17 11:11:56 +0530

    test: button componnet

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,13 @@
-<script setup lang="ts"></script>
-<template></template>   
+<script setup lang="ts">
+import Buttons from "./components/Buttons.vue";
+</script>
+<template>
+  <Buttons
+    icon="mdi-plus"
+    text="Add"
+    color="white"
+    :border-radius="12"
+    background-color="blue"
+    width="200px"
+  />
+</template>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface ButtonProps {
+  text: string;
+  color: string;
+  borderRadius: number;
+  backgroundColor: string;
+  icon?: string | null;
+  width?: string;
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+  width: "100%",
+  icon: null,
+});
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+
+const onClick = () => {
+  emit("click");
+};
+</script>
+
+<template>
+  <v-btn
+    :color="color"
+    :width="width"
+    :style="{
+      borderRadius: borderRadius + 'px',
+      backgroundColor: backgroundColor,
+    }"
+    @click="onClick"
+  >
+    <v-icon v-if="icon !== null" left>
+      {{ icon }}
+    </v-icon>
+    {{ text }}
+  </v-btn>
+</template>
