97eb2e7d2a071fce25a97b4ef880dd835225c791
AvaniSujith <chinchuavani123@gmail.com>
2025-07-17 14:41:14 +0530

    chore: pinia setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
+import { createPinia } from "pinia";
 
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -8,10 +9,12 @@ import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 const vuetify = createVuetify({
   components,
   directives,
 });
 
 app.use(vuetify);
-app.mount("#app"); 
+app.use(pinia);
+app.mount("#app");
