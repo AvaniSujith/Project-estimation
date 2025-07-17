524eaa56d6fb2c03d2305c3e51081a0efb177361
AvaniSujith <chinchuavani123@gmail.com>
2025-07-17 11:12:47 +0530

    feat: button component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,2 @@
-<script setup lang="ts">
-import Buttons from "./components/Buttons.vue";
-</script>
-<template>
-  <Buttons
-    icon="mdi-plus"
-    text="Add"
-    color="white"
-    :border-radius="12"
-    background-color="blue"
-    width="200px"
-  />
-</template>
+<script setup lang="ts"></script>
+<template></template>
